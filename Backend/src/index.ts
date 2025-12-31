213b96bf98547508d09e1565fecd2248271cbde7
MastrStudios <152288725+Techyhub-stack@users.noreply.github.com>
2026-01-01 00:36:59 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,37 +1,36 @@
-import currencyRoute from "./routes/currency";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/auth";
-import { authMiddleware, AuthRequest } from "./middleware/auth";
 import productRoutes from "./routes/products";
 import statsRoute from "./routes/stats";
 import locationRoute from "./routes/location";
-
+import currencyRoute from "./routes/currency";
+import { authMiddleware, AuthRequest } from "./middleware/auth";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-app.use("/auth", authRoutes);
 
 app.get("/", (_, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/stats", statsRoute);
+app.use("/location", locationRoute);
+app.use("/currency", currencyRoute);
 
 app.get("/profile", authMiddleware, (req: AuthRequest, res) => {
   res.json({
     message: "Access granted",
-    userId: req.userId
+    userId: req.userId,
   });
 });
 
-app.use("/products", productRoutes);
-
-app.use("/stats", statsRoute);
-app.use("/location", locationRoute);
-
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
